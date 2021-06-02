857870cd39e914b3ffdd0d3f70f9972625130094
bharathkkb <bharathkrishnakb@gmail.com>
2021-06-02 14:34:31 -0500

    switch backend name

--- gh/repos/test_infra_sources.tf
+++ gh/repos/test_infra_sources.tf
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 Google LLC
+ * Copyright 2021 Google LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -66,7 +66,7 @@ data "terraform_remote_state" "deploy-workflow-infra" {
   backend = "gcs"
   config = {
     bucket = "actions-infra-tfstate-b4fa"
-    prefix = "state/deploy-workflow-test-infra"
+    prefix = "state/deploy-workflow-infra"
   }
 }
 
@@ -74,7 +74,7 @@ data "terraform_remote_state" "setup-sdk-infra" {
   backend = "gcs"
   config = {
     bucket = "actions-infra-tfstate-b4fa"
-    prefix = "state/setup-sdk-test-infra"
+    prefix = "state/setup-sdk-infra"
   }
 }
 
