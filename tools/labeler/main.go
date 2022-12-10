29c35ca81afc96880aad1bfa3da30bd5e69459a6
Seth Vargo <seth@sethvargo.com>
2022-12-09 21:35:24 -0500

    Add labeler

---
+++ tools/labeler/main.go
@@ -0,0 +1,126 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/google/go-github/v48/github"
+	"github.com/hashicorp/go-multierror"
+	"golang.org/x/oauth2"
+)
+
+type Label struct {
+	// Name is the name of the label. It should be lowercase to match our standards.
+	Name string
+
+	// Description is the description of the label, with no trailing period.
+	Description string
+
+	// Color is the hex color of the label, without the leading "#".
+	Color string
+}
+
+// DefaultLabels is the authorative list of labels.
+var DefaultLabels = []*Label{
+	{"bug", "Something isn't working", "c5221f"},
+	{"documentation", "Improvements or additions to documentation", "1967d2"},
+	{"enhancement", "New feature or request", "ceead6"},
+	{"good first issue", "Good for newcomers", "188038"},
+	{"help wanted", "Extra attention is needed", "feefc3"},
+}
+
+func main() {
+	ctx := context.Background()
+
+	tokenSource := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: os.Getenv("GITHUB_TOKEN")},
+	)
+
+	client := github.NewClient(oauth2.NewClient(ctx, tokenSource))
+
+	repos, _, err := client.Repositories.List(ctx, "google-github-actions", &github.RepositoryListOptions{
+		ListOptions: github.ListOptions{
+			PerPage: 100,
+		},
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	for _, repo := range repos {
+		fmt.Printf("syncing %s...\n", *repo.Name)
+		if err := syncLabels(ctx, client, "google-github-actions", *repo.Name); err != nil {
+			panic(err)
+		}
+	}
+}
+
+func syncLabels(ctx context.Context, client *github.Client, owner, repo string) error {
+	labels, _, err := client.Issues.ListLabels(ctx, owner, repo, &github.ListOptions{PerPage: 50})
+	if err != nil {
+		return fmt.Errorf("failed to list labels: %w", err)
+	}
+
+	labelsCopy := make(map[string]*Label, len(DefaultLabels))
+	for _, v := range DefaultLabels {
+		labelsCopy[v.Name] = &Label{
+			Name:        v.Name,
+			Description: v.Description,
+			Color:       v.Color,
+		}
+	}
+
+	toDelete := make([]*github.Label, 0, len(labels))
+	toUpdate := make([]*github.Label, 0, len(labels))
+	for _, label := range labels {
+		v, ok := labelsCopy[*label.Name]
+		delete(labelsCopy, *label.Name)
+
+		// An upstream label exists which is not in the allowed set, delete it.
+		if !ok {
+			toDelete = append(toDelete, label)
+			continue
+		}
+
+		if *label.Description != v.Description || *label.Color != v.Color {
+			label.Description = &v.Description
+			label.Color = &v.Color
+			toUpdate = append(toUpdate, label)
+			continue
+		}
+	}
+
+	toCreate := make([]*github.Label, 0, len(labelsCopy))
+	for _, v := range labelsCopy {
+		toCreate = append(toCreate, &github.Label{
+			Name:        &v.Name,
+			Description: &v.Description,
+			Color:       &v.Color,
+		})
+	}
+
+	var merr *multierror.Error
+	for _, v := range toDelete {
+		if _, err := client.Issues.DeleteLabel(ctx, owner, repo, *v.Name); err != nil {
+			fmt.Printf("deleting label %s\n", *v.Name)
+			merr = multierror.Append(merr, fmt.Errorf("failed to delete label %s: %w", *v.Name, err))
+		}
+	}
+
+	for _, v := range toUpdate {
+		if _, _, err := client.Issues.EditLabel(ctx, owner, repo, *v.Name, v); err != nil {
+			fmt.Printf("editing label %s\n", *v.Name)
+			merr = multierror.Append(merr, fmt.Errorf("failed to edit label %s: %w", *v.Name, err))
+		}
+	}
+
+	for _, v := range toCreate {
+		if _, _, err := client.Issues.CreateLabel(ctx, owner, repo, v); err != nil {
+			fmt.Printf("creating label %s\n", *v.Name)
+			merr = multierror.Append(merr, fmt.Errorf("failed to create label %s: %w", *v.Name, err))
+		}
+	}
+
+	return merr.ErrorOrNil()
+}
